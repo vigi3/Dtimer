b1ed8373ca3bd1d1711a00f8d44305e2b5083120
vigi3 <vincentpro52@gmail.com>
2020-08-07 13:58:47 +0200

    Add call to Unsplash API

--- app/src/main/java/com/proj/dtimer/Timer.java
+++ app/src/main/java/com/proj/dtimer/Timer.java
@@ -1,8 +1,10 @@
 package com.proj.dtimer;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +13,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Timer extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONObject;
+
+
+
+public class Timer extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener, Request.Method {
 
     CountDownTimer countDownTimer = null;
     TextView chrono;
     Button startTime;
     int seconds = 0;
-//    float textSize;
+    String token;
+    String url;
+    RequestQueue queue;
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +42,13 @@ public class Timer extends AppCompatActivity implements View.OnClickListener, Vi
         setContentView(R.layout.timer);
         chrono = findViewById(R.id.chrono);
         startTime = findViewById(R.id.startButton);
-//        textSize = chrono.getTextSize();
 
-
+        context = Timer.this;
+        queue = Volley.newRequestQueue(context);
         startTime.setOnTouchListener(this);
         chrono.setOnClickListener(this);
+        token = getString(R.string.UNSPLASH_API_TOKEN);
+        url = "https://api.unsplash.com/photos/random/?client_id=" + token;
 
 
     }
@@ -75,8 +94,36 @@ public class Timer extends AppCompatActivity implements View.OnClickListener, Vi
     public boolean onTouch(View v, MotionEvent event) {
         if (MotionEvent.ACTION_UP == event.getAction())
             startTimer();
+            imageCall();
         return false;
     }
+
+
+    // API call
+
+    public void imageCall(){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("Yup", "Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Yup", "ERROR: " + error);
+            }
+
+        });
+        queue.add(jsonObjectRequest);
+
+
+    }
+
+
+
+
+
+
 }
 
 
