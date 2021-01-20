cd906671ca822fdfce30a48e2420a8fa5f27a5f3
vigi3 <vincentpro52@gmail.com>
2021-01-20 11:36:14 +0100

    You can read all Tasks for one project.

--- app/src/main/java/com/proj/dtimer/ScrollingTaskActivity.java
+++ app/src/main/java/com/proj/dtimer/ScrollingTaskActivity.java
@@ -5,22 +5,28 @@ import android.os.Bundle;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
 import java.util.List;
 
-public class ScrollingTaskActivity extends AppCompatActivity {
+public class ScrollingTaskActivity extends AppCompatActivity implements TextEnterDialogFragment.TextEnterDialogListener {
 
     private DataBaseManager dbInst;
     private ListView taskList;
+    private Tasks tasks;
     int projectIdView;
+    TextInputEditText taskName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +34,7 @@ public class ScrollingTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling_task);
         taskList = findViewById(R.id.taskList);
         projectIdView = getIntent().getIntExtra("EXTRA_PROJECT_ID", 0);
-        Log.e("TAG", "readScroll: " + projectIdView);
+        Log.e("Scrollingtask", "readScroll: " + projectIdView);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,7 +52,7 @@ public class ScrollingTaskActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-
+                textEnterDialogTaskFragment();
             }
         });
 
@@ -60,13 +66,16 @@ public class ScrollingTaskActivity extends AppCompatActivity {
         final List<Tasks> tasks = dbInst.showAllTasksFromOneProject(projectIdView);
 //        final List<Tasks> tasks = dbInst.showAllTasks();
 
-        Log.e("TAG", "readTasks: " + projectIdView);
+        Log.e("ScrollingTask", "readTaskProjectId: " + projectIdView);
 
         if(tasks.isEmpty()){
             taskList.setVisibility(View.GONE);
         }
         else {
             taskList.setVisibility(View.VISIBLE);
+            for (Tasks myTaskList: tasks) {
+                Log.e("ScrollingTask", "readTaskName: " + myTaskList);
+            }
         }
 
         taskList.setAdapter(new TaskListAdapter(this, tasks));
@@ -75,5 +84,39 @@ public class ScrollingTaskActivity extends AppCompatActivity {
 
     }
 
+    public void textEnterDialogTaskFragment() {
+        // Create an instance of the dialog fragment and show it
+        TextEnterDialogFragment textDialog = new TextEnterDialogFragment();
+        textDialog.show(getSupportFragmentManager(), "TextEnterDialogTaskFragment");
+    }
 
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // Positive Event is return by interface TextEnterDialogListener so logic can be done here
+        taskName = dialog.getDialog().findViewById(R.id.textInput);
+        if (TextUtils.isEmpty(taskName.getEditableText())) {
+            // if dialogBox empty, restart dialogBox
+            textEnterDialogTaskFragment();
+            TextInputLayout textInputLayout = dialog.getDialog().findViewById(R.id.textInputLayout);
+            textInputLayout.setHint("Name missing !");
+            Log.e("Dialog", "Task name is null/empty");
+        }
+        else {
+            tasks = new Tasks();
+            dbInst = new DataBaseManager(getApplicationContext());
+            tasks.setName(String.valueOf(taskName.getText()));
+            tasks.setIdProject(projectIdView);
+            tasks.setState(0);
+            dbInst.addTask(tasks);
+            dbInst.close();
+            dialog.dismiss();
+            readTasks(projectIdView);
+        }
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+
+    }
 }
