a60e039ab03b28ea4c9d5e7e0cd1d7d80dee177b
vigi3 <vincentpro52@gmail.com>
2021-02-05 17:32:37 +0100

    Smoother Transition with Shared Element.

--- app/src/main/java/com/proj/dtimer/OneTaskView.java
+++ app/src/main/java/com/proj/dtimer/OneTaskView.java
@@ -7,15 +7,20 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.ContextCompat;
 
 import android.graphics.drawable.Drawable;
-import android.transition.ChangeTransform;
+import android.transition.AutoTransition;
+import android.transition.Explode;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.transition.ChangeBounds;
+import android.transition.Fade;
 import android.transition.Transition;
 import android.transition.TransitionListenerAdapter;
+import android.transition.TransitionSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -23,7 +28,7 @@ import android.widget.TextView;
 public class OneTaskView extends AppCompatActivity {
 
     private String taskNameView;
-    private View includeLayout;
+    private View taskTextView;
     private TextView nameTask;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -34,28 +39,33 @@ public class OneTaskView extends AppCompatActivity {
         //Transition
         getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
         ChangeBounds changeBounds = new ChangeBounds();
-        changeBounds.setDuration(300);
-        changeBounds.setResizeClip(false);
+//        changeBounds.setDuration(300);
+        changeBounds.setResizeClip(true);
+        changeBounds.addTarget("tasksList");
         getWindow().setSharedElementEnterTransition(changeBounds);
         getWindow().setSharedElementReturnTransition(changeBounds);
         getWindow().setEnterTransition(null);
         getWindow().setExitTransition(null);
         getWindow().setAllowEnterTransitionOverlap(true);
         getWindow().setAllowReturnTransitionOverlap(true);
+        postponeEnterTransition();
 
         setContentView(R.layout.activity_one_task_view);
-        includeLayout = findViewById(R.id.projectDialogLayout);
-        includeLayout.setTransitionName("tasksList");
+        taskTextView = findViewById(R.id.taskTextView);
+        taskTextView.setTransitionName("tasksList");
         taskNameView = getIntent().getStringExtra("EXTRA_TASK_NAME");
-        nameTask = includeLayout.findViewById(R.id.detailName);
-        nameTask.setText(taskNameView);
+        TextView textView = (TextView) taskTextView;
+//        textView.setText(taskNameView);
+        scheduleStartPostponedTransition(taskTextView);
         getWindow().getSharedElementEnterTransition().addListener(new TransitionListenerAdapter() {
             @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
             public void onTransitionEnd(Transition transition) {
-                includeLayout.setBackgroundColor(getColor(R.color.colorPrimary));
-                nameTask.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f);
-                TextView textView = includeLayout.findViewById(R.id.percentageCompletion);
-                textView.setVisibility(View.GONE);
+//                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f);
                 transition.removeListener(this);
             }
         });
@@ -63,11 +73,25 @@ public class OneTaskView extends AppCompatActivity {
         getWindow().getSharedElementReturnTransition().addListener(new TransitionListenerAdapter() {
             @Override
             public void onTransitionStart(Transition transition) {
-                includeLayout.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.project_box));
-                nameTask.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f);
+//                nameTask.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f);
 
             }
         });
 
+    }
+
+    //Check that the transition does know the EndValue of the Drawable, to smooth the animation
+    private void scheduleStartPostponedTransition(final View sharedElement) {
+        sharedElement.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        sharedElement.getViewTreeObserver().removeOnPreDrawListener(this);
+                        Log.e("efr", "onPreDraw: true");
+                        startPostponedEnterTransition();
+                        return true;
+                    }
+                }
+        );
     }
 }
